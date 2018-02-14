ff2d2483aa3c9ff2967fceb186b45929bec4bd93
Aathif Sanaya <sanaya.aathif961008@gmail.com>
2018-02-14 18:20:09 +0530

    Update Single_Flex_Sensor_Basic.ino

--- Device/Single_Flex_Sensor_Basic/Single_Flex_Sensor_Basic.ino
+++ Device/Single_Flex_Sensor_Basic/Single_Flex_Sensor_Basic.ino
@@ -1,35 +1,21 @@
 /******************************************************************************
-Flex_Sensor_Example.ino
-Example sketch for SparkFun's flex sensors
-  (https://www.sparkfun.com/products/10264)
-Jim Lindblom @ SparkFun Electronics
-April 28, 2016
 
-Create a voltage divider circuit combining a flex sensor with a 47k resistor.
-- The resistor should connect from A0 to GND.
-- The flex sensor should connect from A0 to 3.3V
-As the resistance of the flex sensor increases (meaning it's being bent), the
-voltage at A0 should decrease.
+SignSpeaks - Sign language translation glove
+Author - Return 0;
 
-Development environment specifics:
-Arduino 1.6.7
 ******************************************************************************/
+
 const int FLEX_PIN1 = A0;
 const int FLEX_PIN2 = A1;
 const int FLEX_PIN3 = A2;
 const int FLEX_PIN4 = A3;
 const int FLEX_PIN5 = A4;
-// Pin connected to voltage divider output
 
-// Measure the voltage at 5V and the actual resistance of your
-// 47k resistor, and enter them below:
-const float VCC = 4.98; // Measured voltage of Ardunio 5V line
-const float R_DIV = 47500.0; // Measured resistance of 3.3k resistor
+const float VCC = 4.98; 
+const float R_DIV = 47500.0; 
 
-// Upload the code, then try to adjust these values to more
-// accurately calculate bend degree.
-const float STRAIGHT_RESISTANCE = 37300.0; // resistance when straight
-const float BEND_RESISTANCE = 90000.0; // resistance at 90 deg
+const float STRAIGHT_RESISTANCE = 37300.0; 
+const float BEND_RESISTANCE = 90000.0; 
 
 float finger1;
 float finger2;
@@ -58,105 +44,8 @@ void loop()
 
   Serial.println("\n");
 
-  if (finger1 >= 1300 && finger1 <= 2300){
-    if(finger2 >= 2300 && finger2 <=3300){
-      if (finger3 >= 1700 && finger3 <= 2700){
-        if(finger4 >= 2500 && finger4 <= 3500){
-          if(finger5 >= 3000 && finger5 <=4100){
-            Serial.println("A");
-            }
-          }
-        }
-      }
-    }
-
-    if (finger1 >= 1500 && finger1 <= 2600){
-    if(finger2 >= 1400 && finger2 <=1700){
-      if (finger3 >= 1400 && finger3 <= 1700){
-        if(finger4 >= 1400 && finger4 <= 1700){
-          if(finger5 >= 1400 && finger5 <=1700){
-            Serial.println("B");
-            }
-          }
-        }
-      }
-    }
-
-     if (finger1 >= 1400 && finger1 <= 1700){
-    if(finger2 >= 1600 && finger2 <=2200){
-      if (finger3 >= 1500 && finger3 <= 2000){
-        if(finger4 >= 1600 && finger4 <= 2500){
-          if(finger5 >= 1600 && finger5 <=2300){
-            Serial.println("C");
-            }
-          }
-        }
-      }
-    }
-
-      if (finger1 >= 1300 && finger1 <= 1500){
-    if(finger2 >= 1800 && finger2 <=2600){
-      if (finger3 >= 1600 && finger3 <= 2200){
-        if(finger4 >= 2000 && finger4 <= 3000){
-          if(finger5 >= 1400 && finger5 <=1600){
-            Serial.println("D");
-            }
-          }
-        }
-      }
-    }
-
-       if (finger1 >= 2000 && finger1 <= 3000){
-    if(finger2 >= 2200 && finger2 <= 2900){
-      if (finger3 >= 1800 && finger3 <= 2300){
-        if(finger4 >= 2300 && finger4 <= 3200){
-          if(finger5 >= 2200 && finger5 <= 3100){
-            Serial.println("E");
-            }
-          }
-        }
-      }
-    }
-
-     if (finger1 >= 1300 && finger1 <= 1600){
-    if(finger2 >= 2000 && finger2 <= 2900){
-      if (finger3 >= 1300 && finger3 <= 1500){
-        if(finger4 >= 1400 && finger4 <= 1700){
-          if(finger5 >= 1400 && finger5 <= 1600){
-            Serial.println("F");
-            }
-          }
-        }
-      }
-    }
-
-    if (finger1 >= 1300 && finger1 <= 1600){
-    if(finger2 >= 1500 && finger2 <= 1800){
-      if (finger3 >= 1700 && finger3 <= 2500){
-        if(finger4 >= 1800 && finger4 <= 3000){
-          if(finger5 >= 2600 && finger5 <= 3100){
-            Serial.println("G");
-            }
-          }
-        }
-      }
-    }
-
-    if (finger1 >= 1500 && finger1 <= 1700){
-    if(finger2 >= 1500 && finger2 <= 1600){
-      if (finger3 >= 1300 && finger3 <= 1500){
-        if(finger4 >= 2800 && finger4 <= 3700){
-          if(finger5 >= 2800 && finger5 <= 3500){
-            Serial.println("H");
-            }
-          }
-        }
-      }
-    }
-
-    
+  Serial.println(getLetter()); 
   
- 
   delay(1500);
 }
 
@@ -174,6 +63,288 @@ float getDetails(int FLEX_PIN){
   
   Serial.println("Resistance: " + String(flexKR) + " K ohms\t\tBend: " + String(angle) + " degrees");
 
-    return flexKR;
+   return flexKR;
  
+}
+
+String getLetter(){
+  
+   if (finger1 >= 1500 && finger1 <= 1700){
+    if(finger2 >= 2300 && finger2 <=3300){
+      if (finger3 >= 1700 && finger3 <= 2700){
+        if(finger4 >= 3000 && finger4 <= 4100){
+          if(finger5 >= 3000 && finger5 <=4900){
+            return "A";
+            }
+          }
+        }
+      }
+    }
+
+    if (finger1 >= 1500 && finger1 <= 2800){
+    if(finger2 >= 1400 && finger2 <=1700){
+      if (finger3 >= 1400 && finger3 <= 1700){
+        if(finger4 >= 1400 && finger4 <= 1700){
+          if(finger5 >= 1400 && finger5 <=1700){
+            return "B";
+            }
+          }
+        }
+      }
+    }
+
+     if (finger1 >= 1400 && finger1 <= 1700){
+    if(finger2 >= 1800 && finger2 <=2200){
+      if (finger3 >= 1500 && finger3 <= 2000){
+        if(finger4 >= 1800 && finger4 <= 2200){
+          if(finger5 >= 1600 && finger5 <=2200){
+            return "C";
+            }
+          }
+        }
+      }
+    }
+
+      if (finger1 >= 1300 && finger1 <= 1700){
+    if(finger2 >= 1800 && finger2 <=2600){
+      if (finger3 >= 1600 && finger3 <= 2200){
+        if(finger4 >= 2000 && finger4 <= 3000){
+          if(finger5 >= 1400 && finger5 <=1700){
+            return "D";
+            }
+          }
+        }
+      }
+    }
+
+       if (finger1 >= 2000 && finger1 <= 4100){
+    if(finger2 >= 2200 && finger2 <= 2900){
+      if (finger3 >= 1800 && finger3 <= 2600){
+        if(finger4 >= 2300 && finger4 <= 3000){
+          if(finger5 >= 2200 && finger5 <= 3000){
+            return "E";
+            }
+          }
+        }
+      }
+    }
+
+     if (finger1 >= 1300 && finger1 <= 1800){
+    if(finger2 >= 2000 && finger2 <= 2900){
+      if (finger3 >= 1300 && finger3 <= 1500){
+        if(finger4 >= 1400 && finger4 <= 1700){
+          if(finger5 >= 1400 && finger5 <= 1600){
+            return "F";
+            }
+          }
+        }
+      }
+    }
+
+    if (finger1 >= 1600 && finger1 <= 1700){
+    if(finger2 >= 1500 && finger2 <= 1900){
+      if (finger3 >= 1700 && finger3 <= 2500){
+        if(finger4 >= 2000 && finger4 <= 3500){
+          if(finger5 >= 2600 && finger5 <= 3700){
+            return "G";
+            }
+          }
+        }
+      }
+    }
+
+    if (finger1 >= 1700 && finger1 <= 1900){
+    if(finger2 >= 1500 && finger2 <= 1700){
+      if (finger3 >= 1300 && finger3 <= 1500){
+        if(finger4 >= 3000 && finger4 <= 4100){
+          if(finger5 >= 2600 && finger5 <= 4100){
+            return "H";
+            }
+          }
+        }
+      }
+    }
+
+    if (finger1 >= 1700 && finger1 <= 2000){
+    if(finger2 >= 2600 && finger2 <= 3200){
+      if (finger3 >= 2000 && finger3 <= 2900){
+        if(finger4 >= 2800 && finger4 <= 4100){
+          if(finger5 >= 1400 && finger5 <= 1700){
+            return "I";
+            }
+          }
+        }
+      }
+    }
+
+     if (finger1 >= 1300 && finger1 <= 1500){
+    if(finger2 >= 1500 && finger2 <= 1700){
+      if (finger3 >= 1300 && finger3 <= 1500){
+        if(finger4 >= 3000 && finger4 <= 3500){
+          if(finger5 >= 2800 && finger5 <= 3500){
+            return "K";
+            }
+          }
+        }
+      }
+    }
+
+    if (finger1 >= 1300 && finger1 <= 1500){
+    if(finger2 >= 1500 && finger2 <= 1700){
+      if (finger3 >= 2200 && finger3 <= 2900){
+        if(finger4 >= 3000 && finger4 <= 4100){
+          if(finger5 >= 3000 && finger5 <= 4100){
+            return "L";
+            }
+          }
+        }
+      }
+    }
+
+      if (finger1 >= 3000 && finger1 <= 4100){
+    if(finger2 >= 2600 && finger2 <= 3200){
+      if (finger3 >= 2000 && finger3 <= 3000){
+        if(finger4 >= 2800 && finger4 <= 3500){
+          if(finger5 >= 3000 && finger5 <= 4100){
+            return "M";
+            }
+          }
+        }
+      }
+    }
+
+     if (finger1 >= 2000 && finger1 <= 2800){
+    if(finger2 >= 2500 && finger2 <= 3000){
+      if (finger3 >= 2000 && finger3 <= 3500){
+        if(finger4 >= 3000 && finger4 <= 4100){
+          if(finger5 >= 3000 && finger5 <= 4100){
+            return "N";
+            }
+          }
+        }
+      }
+    }
+
+      if (finger1 >= 1500 && finger1 <= 1700){
+    if(finger2 >= 2000 && finger2 <= 2500){
+      if (finger3 >= 1800 && finger3 <= 2300){
+        if(finger4 >= 2000 && finger4 <= 3000){
+          if(finger5 >= 2000 && finger5 <= 3000){
+            return "O";
+            }
+          }
+        }
+      }
+    }
+
+    if (finger1 >= 1400 && finger1 <= 1600){
+    if(finger2 >= 1500 && finger2 <= 1600){
+      if (finger3 >= 1700 && finger3 <= 2000){
+        if(finger4 >= 1800 && finger4 <= 2300){
+          if(finger5 >= 1800 && finger5 <= 2100){
+            return "P";
+            }
+          }
+        }
+      }
+    }
+
+     if (finger1 >= 1300 && finger1 <= 1500){
+    if(finger2 >= 1600 && finger2 <= 2100){
+      if (finger3 >= 1800 && finger3 <= 2200){
+        if(finger4 >= 2000 && finger4 <= 3000){
+          if(finger5 >= 2000 && finger5 <= 3000){
+            return "Q";
+            }
+          }
+        }
+      }
+    }
+
+       if (finger1 >= 1700 && finger1 <= 2000){
+    if(finger2 >= 1600 && finger2 <= 1700){
+      if (finger3 >= 1500 && finger3 <= 1700){
+        if(finger4 >= 2500 && finger4 <= 3800){
+          if(finger5 >= 2500 && finger5 <= 3800){
+            return "R";
+            }
+          }
+        }
+      }
+    }
+
+     if (finger1 >= 1800 && finger1 <= 2100){
+    if(finger2 >= 3000 && finger2 <= 3500){
+      if (finger3 >= 2600 && finger3 <= 3000){
+        if(finger4 >= 3000 && finger4 <= 5500){
+          if(finger5 >= 3000 && finger5 <= 5500){
+            return "S";
+            }
+          }
+        }
+      }
+    }
+
+      if (finger1 >= 1300 && finger1 <= 1500){
+    if(finger2 >= 1700 && finger2 <= 2000){
+      if (finger3 >= 2000 && finger3 <= 2500){
+        if(finger4 >= 2500 && finger4 <= 3300){
+          if(finger5 >= 3000 && finger5 <= 4100){
+            return "T";
+            }
+          }
+        }
+      }
+    }
+
+     if (finger1 >= 1500 && finger1 <= 1700){
+    if(finger2 >= 1500 && finger2 <= 1700){
+      if (finger3 >= 1300 && finger3 <= 1500){
+        if(finger4 >= 2200 && finger4 <= 3000){
+          if(finger5 >= 2200 && finger5 <= 2900){
+            return "V";
+            }
+          }
+        }
+      }
+    }
+
+     if (finger1 >= 1700 && finger1 <= 2000){
+    if(finger2 >= 1500 && finger2 <= 1700){
+      if (finger3 >= 1300 && finger3 <= 1500){
+        if(finger4 >= 1500 && finger4 <= 1700){
+          if(finger5 >= 2500 && finger5 <= 4000){
+            return "W";
+            }
+          }
+        }
+      }
+    }
+
+     if (finger1 >= 3000 && finger1 <= 4100){
+    if(finger2 >= 1800 && finger2 <= 2300){
+      if (finger3 >= 2000 && finger3 <= 2500){
+        if(finger4 >= 2500 && finger4 <= 3000){
+          if(finger5 >= 3000 && finger5 <= 3800){
+            return "X";
+            }
+          }
+        }
+      }
+    }
+
+     if (finger1 >= 1300 && finger1 <= 1500){
+    if(finger2 >= 2500 && finger2 <= 3300){
+      if (finger3 >= 2000 && finger3 <= 2600){
+        if(finger4 >= 2500 && finger4 <= 3000){
+          if(finger5 >= 1500 && finger5 <= 1700){
+            return "Y";
+            }
+          }
+        }
+      }
+    }
+
+    return "no letter";
+  
 }
